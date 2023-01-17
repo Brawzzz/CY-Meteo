dd0b852993679aed846d1ab8c02306e33103ba2f
Hugo Weibel <hugo03.weibel@gmail.com>
2023-01-17 10:27:29 +0100

    Add tools.c & tools.h

---
+++ tools.c
@@ -0,0 +1,123 @@
+#include "tools.h"
+
+// verify if the argument file has .csv extension
+int is_csv(char *s){ 
+    char *extension;
+    extension = strrchr(s,'.'); 
+
+    if (extension == NULL){
+        return 0;
+    }
+    
+    if ((extension[0] == '.') && (extension[1] == 'c') && (extension[2] == 's') && (extension[3] == 'v')){
+        return 1;
+    }
+}
+
+// compare two doubles
+int compare_double (const void * a, const void * b){
+    if (*(double*)a > *(double*)b)
+        return 1;
+    else if (*(double*)a < *(double*)b)
+        return -1;
+    else
+        return 0;  
+}
+
+// compare two int
+int compare_int (const void * a, const void * b){
+    if (*(int*)a > *(int*)b)
+        return 1;
+    else if (*(int*)a < *(int*)b)
+        return -1;
+    else
+        return 0;  
+}
+
+// binary search algorithme for ID
+bool binary_search_data_set_ID(Data_Set* array[] , int size , int value , int* index) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high){
+
+        *index = (low + high) / 2;
+
+        if (array[*index] -> id == value){
+            return true;
+        }
+        else if (array[*index] -> id < value){
+            low = *index + 1;
+        }
+        else{
+            high = *index - 1;
+        }
+    }
+    return false;
+}
+
+// binary search algorithme for date (for a tab of char*)
+bool binary_search_date(char* array[] , int size , char* value , int* index) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high){
+
+        *index = (low + high) / 2;
+
+        if (strcmp(array[*index] , value) == 0){
+            return true;
+        }
+        else if (strcmp(array[*index] , value) < 0){
+            low = *index + 1;
+        }
+        else{
+            high = *index - 1;
+        }
+    }
+    return false;
+}
+
+// binary search algorithme for date (for a tab of Data_Set*)
+bool binary_search_data_set_date(Data_Set* array[] , int size , char* value , int* index) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high){
+
+        *index = (low + high) / 2;
+
+        if (strcmp(array[*index] -> date , value) == 0){
+            return true;
+        }
+        else if (strcmp(array[*index] -> date , value) < 0){
+            low = *index + 1;
+        }
+        else{
+            high = *index - 1;
+        }
+    }
+    return false;
+}
+
+// binary search algorithme for date (for a tab of tab of Data_Set*)
+bool binary_search_tab_date(Data_Set** array[] , int size , char* value , int* index) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high){
+
+        *index = (low + high) / 2;
+
+        if (strcmp(array[*index][0] -> date , value) == 0){
+            return true;
+        }
+        else if (strcmp(array[*index][0] -> date , value) < 0){
+            low = *index + 1;
+        }
+        else{
+            high = *index - 1;
+        }
+    }
+    return false;
+}
---
+++ tools.h
@@ -0,0 +1,20 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+# include "Data_Set.h"
+
+int is_csv(char *s);
+
+int compare_double (const void * a, const void * b);
+
+int compare_int (const void * a, const void * b);
+
+bool binary_search_date(char* array[] , int size , char* value , int* index);
+
+bool binary_search_data_set_ID(Data_Set* array[] , int size , int value , int* index);
+
+bool binary_search_data_set_date(Data_Set* array[] , int size , char* value , int* index);
+
+bool binary_search_tab_date(Data_Set** array[] , int size , char* value , int* index);
+
+#endif
